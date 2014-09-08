eb7845808406aba84128ca7b7bfdc3b6ffc08dbb
Dave Snider <dave.snider@gmail.com>
2014-09-08 13:34:54 -0700

    get slider working with store items

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -19,4 +19,36 @@ $(document).ready(function() {
     autoplay: true,
     autoplaySpeed: 5000
   });
+  $('.slide-store').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
