86710ad7110588789389fa263619b0a446135f69
Dave Snider <dave.snider@gmail.com>
2014-09-10 22:36:47 -0700

    just laying stuff out

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -50,4 +50,35 @@ $(document).ready(function() {
       }
     ]
   });
+  $('.home-store-items').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
